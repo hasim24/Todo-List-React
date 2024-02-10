357be274b715a50f7d7113cee20212d69108503d
Quraishi24 <hashimquraishi.sed@gmail.com>
2024-02-10 11:34:58 +0530

    redux

--- src/To-do list/Todoapp.js
+++ src/To-do list/Todoapp.js
@@ -2,50 +2,47 @@ import React from "react";
 import Addtodos from "./Addtodo";
 import Alltodos from "./Alltodos";
 import CompletedTask from "./CompletedTask";
-import { useDispatch } from 'react-redux';
-import { addTask as addTaskAction } from '../Slices/Slice';
-import { finishedTask as finishedTaskAction } from '../Slices/Slice'
-import { taskUp } from "../Slices/Slice"; 
-import { taskDown } from "../Slices/Slice"; 
+import { useDispatch } from "react-redux";
+import { addTask as addTaskAction } from "../Slices/Slice";
+import { finishedTask as finishedTaskAction } from "../Slices/Slice";
+import { taskUp } from "../Slices/Slice";
+import { taskDown } from "../Slices/Slice";
 
 const Todo = () => {
-const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const addTask = (newTask) => {
-    dispatch(addTaskAction(newTask))
+    dispatch(addTaskAction(newTask));
   };
 
-  
   const finishedTask = (index) => {
-    dispatch(finishedTaskAction(index))
-  }
+    dispatch(finishedTaskAction(index));
+  };
   const upTask = (index) => {
-    dispatch(taskUp(index))
+    dispatch(taskUp(index));
   };
 
   const downTask = (index) => {
-    dispatch(taskDown(index))
+    dispatch(taskDown(index));
   };
   return (
     <div className="container">
       <h1>To-do List</h1>
 
-
       <div className="input">
-        <Addtodos addTasks={addTask} />
+        <Addtodos addTasks = {addTask} />
       </div>
 
-
       <ul>
-        <Alltodos upTask={upTask} 
-                  downTask={downTask} 
-                  finishedTask={finishedTask}
-                  />
+        <Alltodos
+          upTask = {upTask}
+          downTask = {downTask}
+          finishedTask = {finishedTask}
+        />
       </ul>
       <ul>
-      <CompletedTask />
+        <CompletedTask />
       </ul>
-       
     </div>
   );
 };
